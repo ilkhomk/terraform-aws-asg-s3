7736dedbadfe05d6b4c0735c952fb0b41ee1a8bf
ilkhomk <i.kamolov1@gmail.com>
2020-08-18 23:01:46 +0000

    new changes

--- S3.tf
+++ S3.tf
@@ -1,4 +1,26 @@
 resource "aws_s3_bucket" "b" {
-  bucket = "june_s3-bucket"
-  acl    = "private"
-  }
+  bucket = "my-tf-s3-bucket2020"
+}
+
+resource "aws_s3_bucket_policy" "b" {
+  bucket = "${aws_s3_bucket.b.id}"
+
+  policy = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Id": "MYBUCKETPOLICY",
+  "Statement": [
+    {
+      "Sid": "IPAllow",
+      "Effect": "Allow",
+      "Principal": "*",
+      "Action": ["s3:GetObject","s3:GetObjectVersion","s3:PutObject"],
+      "Resource": "arn:aws:s3:::my-tf-s3-bucket2020/*",
+      "Condition": {
+         "IpAddress": {"aws:SourceIp": "8.8.8.8/32"}
+      }
+    }
+  ]
+}
+POLICY
+}
